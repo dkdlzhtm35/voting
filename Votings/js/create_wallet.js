1f0d0f8ba79bf2769b5738ebbd16e86997052df5
dkdlzhtm35 <51919926+dkdlzhtm35@users.noreply.github.com>
2019-11-04 13:04:57 +0900

    Add files via upload

---
+++ Votings/js/create_wallet.js
@@ -0,0 +1,7 @@
+
+function generate_wallet() { 
+    alert('generated');
+}
+function vault_click(){
+    alert('saved');
+}
---
+++ Votings/js/voting.js
@@ -0,0 +1,4 @@
+function vote() {
+    alert('투표완료');
+    alert('10이 지급됐습니다. my page에서 확인해주세요');
+}
